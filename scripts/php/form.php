96115b4c6fe8fa99ca78503bc6dbcad38cb8d6b2
Rujul Walvekar <rujul.w@gmail.com>
2020-07-14 02:45:11 +0530

    pull for dani

--- scripts/php/form.php
+++ scripts/php/form.php
@@ -15,7 +15,7 @@
     <div class="middle-container">
         <form action="form2.php" method="POST">
             <label for="roll_no">Roll Number:</label><br>
-            <input type="number" id="roll_no" name="roll_no"required><br>
+            <input type="tel" id="roll_no" name="roll_no"required><br>
             <label for="fname">First name:</label><br>
             <input type="text" id="fname" name="name"required><br>
             <label for="class">Class:</label><br>
